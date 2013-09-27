9e94266c36779450acea7bb2e9b58147c2847a02
Sohil Habib <vsohail09@gmail.com>
2013-09-27 17:05:58 -0400

    Sohil | First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bestbaynavi::Application.config.secret_key_base = '58b1b872a1af3129152757b6e68588866f3c4f12b2f0276309af6a7260e7284037890b836a1411a9e743fe84f5012bcc300dd8e6cd1376405747bfdb45ab5a17'
